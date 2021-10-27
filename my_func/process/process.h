9cc50a07161db406388c44b9276b61bdf460e3f3
Dioq <zhendong2011@live.cn>
2021-10-27 11:18:52 +0800

    ptrace attach and debug

--- my_func/process/process.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-
-char * get_process_name(int pid);
