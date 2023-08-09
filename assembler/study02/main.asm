5c4d7f228097f7cf0f1114a316438e977e3d68dc
free <ptttqq@gmail.com>
2023-08-10 04:00:34 +0800

    aarch64 bl printf function print string

--- assembler/study02/main.asm
+++ assembler/study02/main.asm
@@ -5,9 +5,9 @@ msg:
 .section 	.text
 .global 	main
 main:
-	adrp	x0, msg					// 计算出 page
-	add		x0, x0, :lo12:msg		// :lo12:msg 取 msg 低12位 即页内偏移offset,  msging vaddr = page + offset
-    bl 		printf					// bl 跳转到 printf 子程序
+	adrp	x0, msg					        // 计算出 page
+	add		x0, x0, :lo12:msg		        // :lo12:msg 取 msg 低12位 即页内偏移offset,  msging vaddr = page + offset
+    bl 		printf					        // bl 跳转到 printf 子程序
  
     mov 	x0, #0
     bl 		exit							// bl 跳转到 exit 子程序
