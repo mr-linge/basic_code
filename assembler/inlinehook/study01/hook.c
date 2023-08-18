04118eeb8b7f7b9139b6acd04b5e67d269019973
dioq <ptttqq@gmail.com>
2023-08-18 17:35:36 +0800

    inline hook 优化代码

--- assembler/inlinehook/study01/hook.c
+++ assembler/inlinehook/study01/hook.c
@@ -31,27 +31,27 @@ void hook(void *target_vaddr, void *new_vaddr, void **old_vaddr)
 {
     int status;
     union Register reg = {0}; // 寄存器操作
+    // 52 00 00 58             ldr x18, #8
+    // 40 02 1F D6             br x18
+    unsigned char jump_code[0x8] = {
+        0x52, 0x00, 0x00, 0x58,
+        0x40, 0x02, 0x1F, 0xD6};
 
-    void *old_func = mmap(NULL, 32, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
-    printf("old_func:%p\n", old_func);
-    if (old_func == MAP_FAILED) /* 判断是否映射成功 */
+    void *call_back = mmap(NULL, 0x20, PROT_READ | PROT_WRITE | PROT_EXEC, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+    // printf("call_back:%p\n", call_back);
+    if (call_back == MAP_FAILED) /* 判断是否映射成功 */
     {
         fprintf(stderr, "%s:%d mmap error: %s\n", __FILE__, __LINE__, strerror(errno));
         exit(1);
     }
-    unsigned char back_codes[32] = {0};
-    memcpy(back_codes, target_vaddr, 16);
-    unsigned char jump_code[0x8] = {
-        0x52, 0x00, 0x00, 0x58,
-        0x40, 0x02, 0x1F, 0xD6};
-    memcpy((void *)(back_codes + 16), jump_code, 8);
-    reg.val = (unsigned long)target_vaddr + 16;
+    // 构造回调用函数的指令
+    memcpy(call_back, target_vaddr, 0x10);
+    memcpy((void *)(call_back + 0x10), jump_code, 8);
+    reg.val = (unsigned long)target_vaddr + 0x10; // 跳回原函数要越过前4条指令,因为开头的4条指令被替换了
     // printf("reg.val:0x%lx\n", reg.val);
-    memcpy((void *)(back_codes + 24), reg.bytes, 8);
-    // 写到 heap
-    memcpy(old_func, back_codes, 32);
+    memcpy((void *)(call_back + 24), reg.bytes, 8);
 
-    *old_vaddr = old_func; // 将需要回调的函数指针,指向上面已经构造好的指令处
+    *old_vaddr = call_back; // 将需要回调的函数指针,指向上面已经构造好的指令处
 
     unsigned long target_vaddr_page_start = PAGE_START((unsigned long)target_vaddr);
     // printf("target_vaddr_page_startt:0x%lx\n", target_vaddr_page_start);
@@ -62,14 +62,10 @@ void hook(void *target_vaddr, void *new_vaddr, void **old_vaddr)
         exit(1);
     }
 
-    // 52 00 00 58             ldr x18, #8
-    // 40 02 1F D6             br x18
-    unsigned char jumpCode[0x10] = {
-        0x52, 0x00, 0x00, 0x58,
-        0x40, 0x02, 0x1F, 0xD6};
+    // 目标函数首地址处构造跳转函数
+    memcpy(target_vaddr, jump_code, 8);
     reg.val = (unsigned long)new_vaddr;
-    memcpy((void *)(jumpCode + 8), reg.bytes, 8);
-    memcpy(target_vaddr, jumpCode, 0x10);
+    memcpy((void *)(target_vaddr + 8), reg.bytes, 8);
 
     status = mprotect((void *)target_vaddr_page_start, PAGESIZE, PROT_READ | PROT_EXEC);
     if (status != 0)
@@ -78,7 +74,7 @@ void hook(void *target_vaddr, void *new_vaddr, void **old_vaddr)
         exit(1);
     }
 
-    status = mprotect(old_func, PAGESIZE, PROT_READ | PROT_EXEC);
+    status = mprotect(call_back, PAGESIZE, PROT_READ | PROT_EXEC);
     if (status != 0)
     {
         fprintf(stderr, "%s:%d mprotect error: %s\n", __FILE__, __LINE__, strerror(errno));
