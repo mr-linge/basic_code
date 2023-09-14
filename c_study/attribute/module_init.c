8117d98f101b069154cd7c90e5124f39cd9960c4
dioq <ptttqq@gmail.com>
2023-09-14 18:22:30 +0800

    module_init 优化

--- c_study/attribute/module_init.c
+++ c_study/attribute/module_init.c
@@ -2,32 +2,32 @@
 
 __attribute__((constructor(103))) void pre_proc_3(void)
 {
-	printf("%s %d\n", __FUNCTION__, 103);
+	printf("%s prio:%d\n", __FUNCTION__, 103);
 }
 
 __attribute__((destructor(103))) void end_proc_3(void)
 {
-	printf("%s %d\n", __FUNCTION__, 103);
+	printf("%s prio:%d\n", __FUNCTION__, 103);
 }
 
 __attribute__((constructor(102))) void pre_proc_2(void)
 {
-	printf("%s %d\n", __FUNCTION__, 102);
+	printf("%s prio:%d\n", __FUNCTION__, 102);
 }
 
 __attribute__((destructor(102))) void end_proc_2(void)
 {
-	printf("%s %d\n", __FUNCTION__, 102);
+	printf("%s prio:%d\n", __FUNCTION__, 102);
 }
 
 __attribute__((constructor(101))) void pre_proc_1(void)
 {
-	printf("%s %d\n", __FUNCTION__, 101);
+	printf("%s prio:%d\n", __FUNCTION__, 101);
 }
 
 __attribute__((destructor(101))) void end_proc_1(void)
 {
-	printf("%s %d\n", __FUNCTION__, 101);
+	printf("%s prio:%d\n", __FUNCTION__, 101);
 }
 
 __attribute__((constructor)) void before_main()
